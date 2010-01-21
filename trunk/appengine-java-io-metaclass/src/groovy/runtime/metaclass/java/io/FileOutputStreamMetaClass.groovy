d9cabc6cb19792d695201e1f38184e9a58395699
limcheekin@vobject.com <limcheekin@vobject.com@66383890-e7f3-11de-b362-3513cf04e977>
2010-01-21 16:15:14 +0000

    appengine-java-io-metaclass (DelegatingMetaClass) allow appengine-java-io (gaevfs ported JavaIO API) to be used in groovy project as drop-in jar file (auto register).

---
+++ trunk/appengine-java-io-metaclass/src/groovy/runtime/metaclass/java/io/FileOutputStreamMetaClass.groovy
@@ -0,0 +1,41 @@
+/*
+ * The VObject AppEngine Java-IO is an open source project that porting gaevfs 
+ * to java.io API to enable java.io classes support in Google App Engine (GAE). 
+ * Copyright (C) VObject.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author: Lim Chee Kin (limcheekin@vobject.com)
+ *
+ */
+package groovy.runtime.metaclass.java.io;
+
+import static groovy.runtime.metaclass.com.vobject.PrintUtil.*
+
+
+class FileOutputStreamMetaClass extends DelegatingMetaClass {
+	private static final String IMPLEMENT_CLASS_NAME = "com.vobject.appengine.java.io.FileOutputStream"
+	
+	FileOutputStreamMetaClass(MetaClass delegate) {
+		super(delegate);
+	}
+	
+	Object invokeConstructor(Object[] arguments) {
+		printInvokeMethodEnter("invokeConstructor", null, null, arguments)
+		Object returnValue = Class.forName(IMPLEMENT_CLASS_NAME).newInstance (arguments)
+		printInvokeMethodExit("invokeConstructor", returnValue)
+		return returnValue
+	}	
+}
